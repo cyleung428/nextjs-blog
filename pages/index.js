ee8c1088634e8f626bf5b1be5611a0cd4969c580
cyleung428 <msf96a85@gmail.com>
2020-07-10 00:43:45 +0800

    feat: import material ui list for blog list

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
 import Date from "../components/date";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import { makeStyles } from "@material-ui/core/styles";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -14,7 +17,28 @@ export async function getStaticProps() {
   };
 }
 
+function ListItemLink(props) {
+  return <ListItem button component="a" {...props} />;
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    paddingLeft: "16px",
+    paddingRight: "16px",
+    fontSize: "1.2rem",
+    lineHeight: "1.5"
+  },
+  title: {
+    fontSize: "1.5rem",
+    lineHeight: 1.4,
+    margin: "1rem 0",
+    paddingLeft: "16px",
+    paddingRight: "16px",
+  }
+}));
+
 export default function Home({ allPostsData }) {
+  const classes = useStyles();
   return (
     <>
       <script src="/prism.js"></script>
@@ -22,7 +46,7 @@ export default function Home({ allPostsData }) {
         <Head>
           <title>Ching Yin's Blog</title>
         </Head>
-        <section className={utilStyles.headingMd}>
+        <section className={classes.root}>
           <p>
             Hello, I’m <b>Ching Yin</b>. I’m currently a year 3 colleague
             studying CS in CityU. I share my news recently on this blog. If you
@@ -35,19 +59,23 @@ export default function Home({ allPostsData }) {
           </p>
         </section>
         <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <h2 className={classes.title}>Blog</h2>
           <ul className={utilStyles.list}>
-            {allPostsData.map(({ id, date, title }) => (
-              <li className={utilStyles.listItem} key={id}>
-                <Link href="/posts/[id]" as={`/posts/${id}`}>
-                  <a>{title}</a>
-                </Link>
-                <br />
-                <small className={utilStyles.lightText}>
-                  <Date dateString={date} />
-                </small>
-              </li>
-            ))}
+            <List component="nav" aria-label="main mailbox folders">
+              {allPostsData.map(({ id, date, title }) => (
+                <ListItemLink href={"/posts/" + id}>
+                  <li className={utilStyles.listItem} key={id}>
+                    <Link href="/posts/[id]" as={`/posts/${id}`}>
+                      <a>{title}</a>
+                    </Link>
+                    <br />
+                    <small className={utilStyles.lightText}>
+                      <Date dateString={date} />
+                    </small>
+                  </li>
+                </ListItemLink>
+              ))}
+            </List>
           </ul>
         </section>
       </Layout>
