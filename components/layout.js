01c2b1b5c3e1c1fd72e82214ac397eeb0de17366
cyleung428 <msf96a85@gmail.com>
2020-09-22 17:16:16 +0800

    fix: update the description

--- components/layout.js
+++ components/layout.js
@@ -13,7 +13,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Ching Yin's own blog, power of Next.js"
         />
         <meta
           property="og:image"
